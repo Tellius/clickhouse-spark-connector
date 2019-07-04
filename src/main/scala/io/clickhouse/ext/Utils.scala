ad02abd27d5834fd0038ba5903a33b4d775daaaf
Shashank <shashankgowda.me@gmail.com>
2019-07-04 12:46:18 +0530

    TPD-8631 Added resource restriction for writing data to Clickhouse

--- src/main/scala/io/clickhouse/ext/Utils.scala
+++ src/main/scala/io/clickhouse/ext/Utils.scala
@@ -1,6 +1,8 @@
 package io.clickhouse.ext
 
 object Utils {
+  
+
   def using[A, B <: {def close(): Unit}] (closeable: B) (f: B => A): A =
     try {
       f(closeable)
@@ -8,4 +10,18 @@ object Utils {
     finally {
       closeable.close()
     }
+
+  def bucketise(maxValue:Int, size:Int):List[(Int, Int)] = {
+    if(size > maxValue) return List((0, maxValue))
+    val values = (0 to maxValue).toList
+    val rangeBoundaries = (0 to maxValue by size).toList
+    val lastRange = if (rangeBoundaries.max < maxValue)
+      List(List(rangeBoundaries.max, maxValue))
+    else
+      List.empty[List[Int]]
+    val ranges = rangeBoundaries.sliding(2,1).toList ++ lastRange
+    ranges.map(range => {
+      (range(0), range(1))  
+    })
+  }
 }
